fa46c8e015ed30340efe17869cba94a0421373b9
Aaditya Kalsi <akalsi.dev@gmail.com>
2016-11-03 00:29:14 -0400

    Remove unneeded prints from irq_handler

--- cpu/isr.c
+++ cpu/isr.c
@@ -30,11 +30,11 @@ void irq_handler(registers r)
     // alert master
     portwriteb(0x20, 0x20);
 
-    debugwritestr("IRQ ");
-    char_t hex[HEX_PRINT_CHARS];
-    bufprinthex(hex, r.int_no);
-    debugwritestr(hex);
-    debugwritestr(" called!\n");
+    // debugwritestr("IRQ ");
+    // char_t hex[HEX_PRINT_CHARS];
+    // bufprinthex(hex, r.int_no);
+    // debugwritestr(hex);
+    // debugwritestr(" called!\n");
 
     {// run handler
         irq h = HANDLERS[r.int_no];
